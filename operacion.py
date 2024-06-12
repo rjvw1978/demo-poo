3da5a2d007f1785843a46b5391d51f6b2704a8d9
Rojas Córsico Ivana Soledad <irojas@gmail.com>
2024-06-12 18:47:47 -0300

    Se agregaron las clases operacion  y suma

---
+++ operacion.py
@@ -0,0 +1,25 @@
+class Operacion:
+    def __init__(self, num1, num2):
+        self.__num1=num1
+        self.__num2=num2
+
+    def get_num1(self):
+        return self.__num1
+    def set_num1(self, num1):
+        self.__num1=num1
+
+    def get_num2(self):
+        return self.__num2
+    def set_num2(self, num2):
+        self.__num2= num2
+
+class Suma(Operacion):
+    def suma(self):
+        return self.get_num1() + self.get_num2()
+    
+
+
+    
+
+    
+        

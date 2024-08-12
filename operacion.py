d04e126d0b72275b2b569189ced0fbdf52622454
Escuela 713 <50681817+rjvw1978@users.noreply.github.com>
2024-08-12 18:40:35 -0300

    Jose modifico el archivo operacion.py (develop)

--- operacion.py
+++ operacion.py
@@ -1,7 +1,7 @@
 class Operacion:
-    def __init__(self, num1, num2):
-        self.__num1=num1
-        self.__num2=num2
+    def __init__(self, num01, num02):
+        self.__num1=num01
+        self.__num2=num02
 
     def get_num1(self):
         return self.__num1
